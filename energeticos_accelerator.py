3c6c77ee2621e92c044a61ffdc7a248523947223
anna <carolinanna93@gmail.com>
2021-07-18 23:40:34 -0300

    Revert "add comments"
    This reverts commit 72b3d76d

--- energeticos_accelerator.py
+++ energeticos_accelerator.py
@@ -1,41 +1,33 @@
-# Define a função que calcula o total geral
 def compute_total(value=0, ornament=False):
     result = value + icms(value) + ipi(value) + pis(value) + cofins(value)
     return result if ornament is False else format_value(result)
 
 
-# Define a função que calcula o imposto ICMS sobre o valor da mercadoria
 def icms(value=0, ornament=False):
     result = value * 18/100
     return result if ornament is False else format_value(result)
 
 
-# Define a função que calcula o imposto IPI sobre o valor da mercadoria
 def ipi(value=0, ornament=False):
     result = value * 4/100
     return result if ornament is False else format_value(result)
 
 
-# Define a função que calcula o imposto PIS sobre o valor da mercadoria
 def pis(value=0, ornament=False):
     result = value * 1.86/100
     return result if ornament is False else format_value(result)
 
 
-# Define a função que calcula o imposto COFINS sobre o valor da mercadoria
 def cofins(value=0, ornament=False):
     result = value * 8.54/100
     return result if ornament is False else format_value(result)
 
 
-# Define uma função para formatar os valores monetários
 def format_value(value=0, coin='R$'):
     return f'{coin}{value:.2f}'.replace('.', ',')
 
 
-# Cria uma lista para receber os dados temporariamente
 data = list()
-# Cria uma lista que receberá os dados da lista data
 data_clients = list()
 
 print('===== BEM-VINDO(a) AO SISTEMA DE CÁLCULO DE TRIBUTAÇÃO =====\n'
@@ -44,13 +36,12 @@ print('===== BEM-VINDO(a) AO SISTEMA DE CÁLCULO DE TRIBUTAÇÃO =====\n'
 print()
 
 while True:
-    # Recebe o nome do cliente, quantidade de enérgicos e guarda na lista data
     data.append(input("Cliente: "))
     amount_commodity = int(input("Quantidade da mercadoria comprada: "))
-    price_commodity = amount_commodity * 4.50   # Calcula o valor da mercadoria
+    price_commodity = amount_commodity * 4.50
     data.append(price_commodity)
-    data_clients.append(data[:])    # Adiciona os dados da lista data para a data_clients
-    data.clear()    # Limpa a lista data
+    data_clients.append(data[:])
+    data.clear()
 
     check = input('Cadastrar outro cliente? [S/N] ').lower()
     if not check == 's':
@@ -63,12 +54,10 @@ total_general = 0
 for client in data_clients:
     name, price_commodity = client
 
-    # Calcula o total geral, total das taxas e total do valor das mercadorias
     total_general += compute_total(price_commodity)
     total_taxes += compute_total(price_commodity) - price_commodity
     total_commodity += price_commodity
 
-    # Mostra todos os resultados na tela
     print(f"Cliente: {name}\n"
           f"ICMS: {icms(price_commodity, True)}; IPI: {ipi(price_commodity, True)}; "
           f"PIS: {pis(price_commodity, True)}; COFINS: {cofins(price_commodity, True)}; "
